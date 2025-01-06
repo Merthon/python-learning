11c84f15e0743583c52acf9bd383aec4a9b619fa
Merthon <imchenxing42@gmail>
2025-01-07 01:08:01 +0800

    add list

--- sequence/delList.py
+++ sequence/delList.py
@@ -1,7 +1,4 @@
 # 从列表中删除元素，只需要使用del语句即可。
-
 my_list = [1, 2, 3, 4, 5]
-
 del my_list[2]
-
 print(my_list)  # [1, 2, 4, 5]
---
+++ sequence/useList.py
@@ -0,0 +1,10 @@
+# 定义一个空的列表
+shopping_list = []
+
+shopping_list.append("帽子")
+shopping_list.append("上衣")
+shopping_list.append("裤子")
+shopping_list.append("鞋子")
+
+
+print(shopping_list)
